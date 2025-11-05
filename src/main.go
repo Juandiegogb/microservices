ce6c532524ea26fd40482fac02f9da21b04962c7
Juan Diego <95363339+Juandiegogb@users.noreply.github.com>
2025-11-05 12:39:09 -0500

    Add new endpoint

--- src/main.go
+++ src/main.go
@@ -12,6 +12,11 @@ func main() {
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{"message": "pong", "request_id": uuid.New().String()})
 	})
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{"status": "ok"})
+	})
+
 	router.Run(":9000")
 
 }
